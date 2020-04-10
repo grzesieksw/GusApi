206ec59e3a3bde9a4b7c2635ab700fe4aee60ce5
skorpian@interia.pl <skorpian@interia.pl>
2020-04-10 18:56:56 +0200

    Dodaj pliki projektów.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using GusApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObslugaGus gus = new ObslugaGus("twojKluczApi");
+
+            var podmiot = gus.PobierzDanePodmiotu("twojnip");
+        }
+    }
+}
---
+++ GusApi/ObslugaGus.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Xml.Serialization;
+using Gus;
+using GusApi.Models;
+using WcfCoreMtomEncoder;
+
+namespace GusApi
+{
+    public class ObslugaGus
+    {
+        private readonly UslugaBIRzewnPublClient GusServices;
+        private readonly string apiKey;
+        private string sessionId;
+
+        public ObslugaGus(string apiKey)
+        {
+            this.apiKey = apiKey;
+            GusServices = new UslugaBIRzewnPublClient();
+            SetupBinding();
+        }
+
+        public PodmiotGus PobierzDanePodmiotu(string nip)
+        {
+            LoginIfRequired();
+
+            ParametryWyszukiwania nipData = new ParametryWyszukiwania();
+            nipData.Nip = nip;
+
+            try
+            {
+                string daneSzukajResponse = GusServices.DaneSzukajPodmioty(nipData);
+
+                using (var reader = new StringReader(daneSzukajResponse))
+                {
+                    XmlRootAttribute xRoot = new XmlRootAttribute();
+                    xRoot.ElementName = "root";
+
+                    XmlSerializer daneSzukajSerializer = new XmlSerializer(typeof(DaneGus), xRoot);
+                    var daneGus = (DaneGus) daneSzukajSerializer.Deserialize(reader);
+
+                    return daneGus.dane;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return null;
+        }
+
+        private void LoginIfRequired()
+        {
+            if (GusServices.GetValue("StatusSesji") == "0") Login();
+        }
+
+        private void Login()
+        {
+            sessionId = GusServices.Zaloguj(apiKey);
+
+            OperationContextScope scope = new OperationContextScope(GusServices.InnerChannel);
+
+            HttpRequestMessageProperty requestProperties = new HttpRequestMessageProperty();
+            requestProperties.Headers.Add("sid", sessionId);
+            OperationContext.Current.OutgoingMessageProperties[HttpRequestMessageProperty.Name] = requestProperties;
+
+        }
+
+        public void Logout()
+        {
+            GusServices.Wyloguj(sessionId);
+        }
+
+        private void SetupBinding()
+        {
+            var encoding = new MtomMessageEncoderBindingElement(new TextMessageEncodingBindingElement());
+            var transport = new HttpsTransportBindingElement();
+
+            var customBinding = new CustomBinding(encoding, transport);
+
+            GusServices.Endpoint.Binding = customBinding;
+        }
+    }
+}
